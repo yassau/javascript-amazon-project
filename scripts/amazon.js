e0c29ccb9b951db6dcf7d8279a99a9448bc1da0f
Yassine Lachguar <yassau@gmail.com>
2023-10-31 13:01:30 +0100

    Generate HTML with JS

---
+++ scripts/amazon.js
@@ -0,0 +1,5 @@
+const products = [{
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {stars: 4.5, count: 87}
+}];
