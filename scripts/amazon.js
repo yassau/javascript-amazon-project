662d736ae111817a63299cbcaaa527c5ffc103eb
Yassine Lachguar <yassau@gmail.com>
2023-10-31 14:52:35 +0100

    add quantity selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -58,6 +58,7 @@ document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
+            const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
             let matchingItem;
             
             cart.forEach((item) => {
@@ -67,11 +68,11 @@ document.querySelectorAll('.js-add-to-cart')
             });
 
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                matchingItem.quantity += productQuantity;
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: productQuantity,
                 });
             }
 
